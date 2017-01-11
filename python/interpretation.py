406897fac839f4afcd5c0bb2c08178ed1d003978
Emanuele Usai <ema2487@gmail.com>
2017-01-11 02:13:40 +0100

    theory interpretation 2

--- python/interpretation.py
+++ python/interpretation.py
@@ -137,34 +137,49 @@ theory_files=['ZPRIME_CS_lhc13tev_NNPDF30_nnlo_as_0118_q=0.5m.txt',
 'ZPRIME_CS_lhc13tev_NNPDF30_nnlo_as_0118_q=2.0m.txt']
 
 #theory1110=[1.4,0.38,0.021]
-theory1110=[1.4,1.87,1.57,1.28,1.01,0.38,0.34,0.258,0.18,0.132,0.106,0.042,0.021]
-width1110=[47,60.3,73.9,87.6,101,170,237,286,355,408,452,872,1050]
-x1110=[1.5,1.6,1.7,1.8,1.9,2.0,2.05,2.1,2.2,2.3,2.4,2.45,2.5]
+theory1110_pre=[1.4,1.87,1.57,1.28,1.01,0.38,0.34,0.258,0.18,0.132,0.106,0.042,0.021]
+width1110_pre=[47,60.3,73.9,87.6,101,170,237,286,355,408,452,872,1050]
+x1110_pre=[1.5,1.6,1.7,1.8,1.9,2.0,2.05,2.1,2.2,2.3,2.4,2.45,2.5]
 #theory1410=[0.12*0.0494,0.03*0.2199,0.0087*0.1014]
 
-theory1410_zp=[0.1096, 0.0941941, 0.0812049, 0.0702895, 
- 0.0610887, 0.0531233, 0.0462985, 
- 0.040349, 0.035255, 0.0309674, 0.0272837,
-  0.0239634, 0.0210468, 0.018506, 
- 0.0162989, 0.0143873, 0.0127257, 
- 0.0112562, 0.00997291, 0.00885648, 
- 0.00788735]
-width1410=[50.5522, 55.6905, 61.152, 67.0003, 
- 73.239, 79.8582, 86.8438, 94.1805, 
- 101.853, 109.845, 118.143, 126.731, 
- 135.594, 144.72, 154.095, 163.705, 
- 173.537, 183.58, 193.822, 453.186, 
- 564.14]
-theory1410_br=[4.94822, 7.86529, 10.3043, 12.4474, 
- 14.3494, 16.0392, 17.5389, 18.8675, 
- 20.0425, 21.0798, 21.9938, 22.7977, 
- 23.5031, 24.1204, 24.6588, 25.1265, 
- 25.5309, 25.8783, 26.1744, 11.9094, 
- 10.1431]
+# theory1410_zp=[0.1096, 0.0941941, 0.0812049, 0.0702895, 
+#  0.0610887, 0.0531233, 0.0462985, 
+#  0.040349, 0.035255, 0.0309674, 0.0272837,
+#   0.0239634, 0.0210468, 0.018506, 
+#  0.0162989, 0.0143873, 0.0127257, 
+#  0.0112562, 0.00997291, 0.00885648, 
+#  0.00788735]
+# width1410=[50.5522, 55.6905, 61.152, 67.0003, 
+#  73.239, 79.8582, 86.8438, 94.1805, 
+#  101.853, 109.845, 118.143, 126.731, 
+#  135.594, 144.72, 154.095, 163.705, 
+#  173.537, 183.58, 193.822, 453.186, 
+#  564.14]
+# theory1410_br=[4.94822, 7.86529, 10.3043, 12.4474, 
+#  14.3494, 16.0392, 17.5389, 18.8675, 
+#  20.0425, 21.0798, 21.9938, 22.7977, 
+#  23.5031, 24.1204, 24.6588, 25.1265, 
+#  25.5309, 25.8783, 26.1744, 11.9094, 
+#  10.1431]
+from theory_data import theory1410_zp,width1410,theory1410_br
 theory1410=[theory1410_zp[i]*theory1410_br[i]/100 for i in range(len(theory1410_zp))]
-x1410=range(1500,2550,50)
+x1410=range(1500,2501,1)
+width1410_rel=[100.0*width1410[i]/x1410[i] for i in range(len(x1410))]
 x1410=[i/1000.0 for i in x1410]
+nwa1410=0
+for i in range(len(width1410_rel)):
+	if width1410_rel[i]>10:
+		nwa1410=i
+		break
+x_1410=array('d',x1410[:nwa1410+1])
+x_1410_2=array('d',x1410[nwa1410:])
+y_theory1410=array('d',theory1410[:nwa1410+1])
+y_theory1410_2=array('d',theory1410[nwa1410:])
+theory_curve_1410=TGraph(len(theory1410[:nwa1410+1]),x_1410,y_theory1410)
+theory_curve_1410_2=TGraph(len(theory1410[nwa1410:]),x_1410_2,y_theory1410_2)
 
+#print width1410_rel
+#print x1410[nwa1410],width1410[nwa1410],width1410_rel[nwa1410]
 
 theory_values=[]
 for i in theory_files:
@@ -192,22 +207,40 @@ x=array('d',[1.5,2.0,2.5])
 # x_1110_2=array('d',x1110[6:])
 # y_theory1110=array('d',theory1110[:7])
 # y_theory1110_2=array('d',theory1110[6:])
-x_1110=array('d',x1110)
-x_1110_2=array('d',x1110[6:])
-y_theory1110=array('d',theory1110)
-y_theory1110_2=array('d',theory1110[6:])
+# x_1110=array('d',x1110)
+# x_1110_2=array('d',x1110[6:])
+# y_theory1110=array('d',theory1110)
+# y_theory1110_2=array('d',theory1110[6:])
 
-x_1410=array('d',x1410)
-y_theory1410=array('d',theory1410)
+# x_1410=array('d',x1410)
+# y_theory1410=array('d',theory1410)
+x1110=range(1500,2501,1)
+x1110=[i/1000.0 for i in x1110]
+x1110_pre_a=array('d',x1110_pre)
+theory1110_pre_a=array('d',theory1110_pre)
+width1110_pre_a=array('d',width1110_pre)
+inter1110=Interpolator(len(x1110_pre), Interpolation.kAKIMA)
+inter1110_w=Interpolator(len(x1110_pre), Interpolation.kAKIMA)
+inter1110.SetData(len(x1110_pre), x1110_pre_a, theory1110_pre_a)
+inter1110_w.SetData(len(x1110_pre), x1110_pre_a, width1110_pre_a)
+width1110=[]
+theory1110=[]
+theory1110_rel=[]
+nwa1110=0
+for i in range(len(x1110)):
+	theory1110.append(inter1110.Eval(x1110[i]))
+	width1110.append(inter1110_w.Eval(x1110[i]))
+	theory1110_rel.append(0.1*width1110[-1]/x1110[i])
+	if theory1110_rel[-1]>10.0 and nwa1110==0:
+		nwa1110=i
 
-inter1110=Interpolator(len(x1110), Interpolation.kAKIMA)
-inter1110_w=Interpolator(len(x1110), Interpolation.kAKIMA)
-inter1110.SetData(len(x1110), x_1110, y_theory1110)
-inter1110_w.SetData(len(x1110), x_1110, y_theory1110)
-
-theory_curve_1110=TGraph(len(theory1110[:7]),x_1110,y_theory1110)
-theory_curve_1110_2=TGraph(len(theory1110[6:]),x_1110_2,y_theory1110_2)
-theory_curve_1410=TGraph(len(theory1410),x_1410,y_theory1410)
+x_1110=array('d',x1110[:nwa1110+1])
+x_1110_2=array('d',x1110[nwa1110:])
+y_theory1110=array('d',theory1110[:nwa1110+1])
+y_theory1110_2=array('d',theory1110[nwa1110:])
+theory_curve_1110=TGraph(len(theory1110[:nwa1110+1]),x_1110,y_theory1110)
+theory_curve_1110_2=TGraph(len(theory1110[nwa1110:]),x_1110_2,y_theory1110_2)
+#theory_curve_1410=TGraph(len(theory1410),x_1410,y_theory1410)
 
 theory_curve_1110.SetLineWidth(3)
 theory_curve_1110.SetLineColor(kAzure)
@@ -223,6 +256,10 @@ theory_curve_1110_33.SetMarkerColor(1)
 theory_curve_1410.SetLineWidth(3)
 theory_curve_1410.SetLineColor(kGreen+3)
 theory_curve_1410.SetMarkerColor(kGreen+3)
+theory_curve_1410_2.SetLineWidth(3)
+theory_curve_1410_2.SetLineColor(kGreen+3)
+theory_curve_1410_2.SetMarkerColor(kGreen+3)
+theory_curve_1410_2.SetLineStyle(7)
 
 zeros_theory=array('d',[0 for i in range(len(theory_values[1]))])
 theory_sigma=TGraphAsymmErrors(len(theory_values[1]),x_theory,y_theory,zeros_theory,zeros_theory,y_theory_down,y_theory_up)
@@ -335,18 +372,19 @@ if doresults:
 
 			if triplet[0]==0.0:
 				theory_curve_1410.Draw('l')
+				theory_curve_1410_2.Draw('l')
 			else:
 				theory_curve_1110.Draw('l')
 				theory_curve_1110_2.Draw('l')
 
-				theory_curve_1110_3=theory_curve_1110.Clone()
-				theory_curve_1110_4=theory_curve_1110_2.Clone()
+				#theory_curve_1110_3=theory_curve_1110.Clone()
+				#theory_curve_1110_4=theory_curve_1110_2.Clone()
 
 				#theory_curve_1110_3.Draw('c')
 				#theory_curve_1110_4.Draw('c')
 			
 
-			legend=TLegend(0.4,0.55,0.9,0.9)
+			legend=TLegend(0.335,0.55,0.9,0.9)
  			legend.SetTextSize(0.030)
   			legend.SetBorderSize(0)
   			legend.SetTextFont(42)
@@ -365,16 +403,16 @@ if doresults:
   			#legend.AddEntry(theory_curve,"SSM Z'#rightarrow Tt, B(Z'#rightarrow Tt) = 70%",'l')
   			#legend.AddEntry(theory_curve_1110,"G*#rightarrow Tt (arXiv:1110.6058)",'l')
   			if not triplet[0]==0.0:
-  				legend.AddEntry(theory_curve_1110,"G*#rightarrow Tt, tan #theta_{3} = 0.44",'l')
+  				legend.AddEntry(theory_curve_1110,"G*#rightarrow Tt, tan #theta_{3} = 0.44, sin #phi_{tR} = 0.6, Y_{*} = 3",'l')
   			#legend.AddEntry(theory_curve_1410,"#rho_{0}#rightarrow Tt (arXiv:1410.2883)",'l')
   			if triplet[0]==0.0:
-  				legend.AddEntry(theory_curve_1410,"#rho^{0}_{L}#rightarrow Tt, y_{L} = c_{3} = 1, g_{#rho_{L}} = 3",'l')
+  				legend.AddEntry(theory_curve_1410,"#rho^{0}_{L}#rightarrow Tt, y = c_{3} = 1, g_{#rho} = 3",'l')
   			if triplet[0]==0.0:
   				#theory_curve_1110_33.SetLineColor(kGreen+3)
-  				legend.AddEntry(theory_curve_1110_33,"#Gamma_{rho^{0}_{L}} > 10 %",'l')
+  				legend.AddEntry(theory_curve_1110_33,"#Gamma_{#rho^{0}_{L}}/m_{#rho^{0}_{L}} > 10 %",'l')
   			else:
   				#theory_curve_1110_33.SetLineColor(kAzure)
-  				legend.AddEntry(theory_curve_1110_33,"#Gamma_{G*} > 10 %",'l')
+  				legend.AddEntry(theory_curve_1110_33,"#Gamma_{G*}/m_{G*} > 10 %",'l')
 
   			legend.Draw()
 			CMS_lumi.CMS_lumi(c, 4, 11)
